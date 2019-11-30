5dfe3633bed7953287140aa55540edfe37c9dfa6
Arturo Lopez Perez <flaab.mrlinux@gmail.com>
2019-11-30 15:50:18 +0100

    bug fix in scaffolding

--- core/built-in/shell/data/scaffold/view/element.php
+++ core/built-in/shell/data/scaffold/view/element.php
@@ -65,47 +65,13 @@ foreach($attributes as $var)
     <a href="<?= $link['controller'] ?>/view/<?= $model->fields->id ?>" title="View">view</a> | 
     <a href="<?= $link['controller'] ?>/update/<?= $model->fields->id ?>" title="Edit">edit</a> | 
     <a data-toggle="modal" data-target="#exampleModalCenter<?= $model->fields->id ?>" href="#">delete</a> 
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModalCenter<?= $model->fields->id ?>" 
-                tabindex="-1" role="dialog" aria-labelledby="ariaModalCenterTitle<?= $model->fields->id ?>" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title text-dark" id="ariaeModalLongTitle<?= $model->fields->id ?>">Confirmation</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body text-dark">
-          Are you sure you want to delete this record?
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
-            <a href="<?= $link['controller'] ?>/delete/<?= $model->fields->id ?>" title="Delete">
-            <button type="button" class="btn btn-danger">Yes, delete this sucker</button>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- End Modal -->
-    <? if(isset($navigate_has_many) && count($navigate_has_many) > 0): ?>
-        | <a class="dropdown-toggle" 
-           href="#" title="Related" 
-           role="button" id="dropdownActionGoTo<?= $model->fields->id ?>"
-           data-toggle="dropdown" 
-           aria-haspopup="true" 
-           aria-expanded="false">related</a>&nbsp;
-           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownActionGoTo<?= $model->fields->id ?>">
-           <? foreach($navigate_has_many as $hasmodel => $target): ?>
-                <a class="dropdown-item small" href="<?= $target ?>/by/<?= strtolower($modelname) ?>/<?= $model->fields->id ?>"><?= ucwords($hasmodel) ?> list</a>
-            <? endforeach; ?>
-          </div>
-    <? endif; ?>
     <? if(isset($model->model_actions) && is_array($model->model_actions) && count($model->model_actions) > 0): ?>
-        | <a class="dropdown-toggle" 
+        | 
+        
+        <div class="d-inline"><a class="dropdown-toggle" 
            href="#" title="Actions" 
-           role="button" id="dropdownActionMenu<?= $model->fields->id ?>"
+           role="button" 
+           id="dropdownActionMenu<?= $model->fields->id ?>"
            data-toggle="dropdown" 
            aria-haspopup="true" 
            aria-expanded="false">actions</a>&nbsp;
@@ -113,7 +79,46 @@ foreach($attributes as $var)
              <? foreach($model->model_actions as $function => $data): ?>
                 <a class="dropdown-item small" href="<?= $link['controller'] ?>/action/<?= $model->fields->id ?>/<?= $function ?>"><?= ucwords($data['name']) ?></a>
             <? endforeach; ?>
-          </div>
+          </div></div>
+    <? endif; ?>
+    <? if(isset($navigate_has_many) && count($navigate_has_many) > 0): ?>
+        | <div class="d-inline"><a class="dropdown-toggle" 
+           href="#" title="Related" 
+           role="button" 
+           id="dropdownActionGoTo<?= $model->fields->id ?>"
+           data-toggle="dropdown" 
+           aria-haspopup="true" 
+           aria-expanded="false">related</a>&nbsp;
+           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownActionGoTo<?= $model->fields->id ?>">
+           <? foreach($navigate_has_many as $hasmodel => $target): ?>
+                <a class="dropdown-item small" href="<?= $target ?>/by/<?= strtolower($modelname) ?>/<?= $model->fields->id ?>"><?= ucwords($hasmodel) ?> list</a>
+            <? endforeach; ?>
+          </div></div>
     <? endif; ?>
 </td>
 </tr>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter<?= $model->fields->id ?>" 
+            tabindex="-1" role="dialog" aria-labelledby="ariaModalCenterTitle<?= $model->fields->id ?>" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-dark" id="ariaeModalLongTitle<?= $model->fields->id ?>">Confirmation</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body text-dark">
+      Are you sure you want to delete this record?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+        <a href="<?= $link['controller'] ?>/delete/<?= $model->fields->id ?>" title="Delete">
+        <button type="button" class="btn btn-danger">Yes, delete this sucker</button>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Modal -->
